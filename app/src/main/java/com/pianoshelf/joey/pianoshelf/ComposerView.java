217aca3d48b5d0388c4d99b981426a47bffe2a1d
joeyjoeyze <joeyjoeyze@gmail.com>
2014-11-10 21:17:52 -0500

    Utilize JSONAdapter to simplify ComposerAdapter

--- app/src/main/java/com/pianoshelf/joey/pianoshelf/ComposerView.java
+++ app/src/main/java/com/pianoshelf/joey/pianoshelf/ComposerView.java
@@ -1,31 +1,24 @@
 package com.pianoshelf.joey.pianoshelf;
 
-import android.app.ActionBar;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Each list item within the listActivity should have a constant height in dp
@@ -35,6 +28,7 @@ public class ComposerView extends ListActivity {
     private String composerUrl;
     private String server;
     private String sheetMusicEndpoint;
+    private int composerCount;
     private JSONArray composers;
     public static final String composerDescription = "A Romantic Composer born in Germany in 1885.";
 
@@ -49,14 +43,19 @@ public class ComposerView extends ListActivity {
         composerUrl = intent.getStringExtra("composersUrl");
         sheetMusicEndpoint = intent.getStringExtra("sheetMusicEndpoint");
 
-        final Context context = getListView().getContext();
-
         // Request to get the array of composers
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
-                (composerUrl, new Response.Listener<JSONArray>(){
-                    public void onResponse(JSONArray response){
-                        composers = response;
-                        setListAdapter(new ComposerAdapter(context, R.layout.activity_composerview_item, composers));
+        JsonObjectRequest composersRequest = new JsonObjectRequest
+                (composerUrl, null, new Response.Listener<JSONObject>(){
+                    public void onResponse(JSONObject response){
+                        try {
+                            //TODO implement infinite scrolling for next/prev pages
+                            composerCount = response.getInt("count");
+                            composers = response.getJSONArray("results");
+                            setListAdapter(new ComposerAdapter(getListView().getContext(),
+                                    R.layout.activity_composerview_item, composers));
+                        } catch (JSONException ex) {
+                            throw new RuntimeException(ex);
+                        }
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -64,7 +63,7 @@ public class ComposerView extends ListActivity {
                         //TODO Something Here
                     }
                 });
-        VolleySingleton.getInstance(this).addToRequestQueue(jsonArrayRequest);
+        VolleySingleton.getInstance(this).addToRequestQueue(composersRequest);
     }
 
     @Override
@@ -74,61 +73,32 @@ public class ComposerView extends ListActivity {
 
 
     // Custom adapter class to handle populating each row
-    private class ComposerAdapter extends BaseAdapter {
-        private Context context;
-        private List<JSONObject> composers;
-        private int layout = R.layout.activity_composerview_item;
-
-        public ComposerAdapter(JSONArray composers) {
-            this.composers = new ArrayList<JSONObject>();
-            for(int i=0;i<composers.length();++i) {
-                try {
-                    this.composers.add(composers.getJSONObject(i));
-                } catch (JSONException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        }
-
+    private class ComposerAdapter extends JSONAdapter {
         public ComposerAdapter(Context context, int layout, JSONArray composers) {
-            this.context = context;
-            this.layout = layout;
-            this.composers = new ArrayList<JSONObject>();
-            for(int i=0;i<composers.length();++i) {
-                try {
-                    this.composers.add(composers.getJSONObject(i));
-                } catch (JSONException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
+            super(context, layout, composers);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
+            View parentView = super.getView(position, convertView, parent);
             // Unwrap the JSONObject
             JSONObject composer;
             String composerPortraitUrl;
             String composerName;
             try {
-                composer = composers.get(position);
+                composer = jsonArray.get(position);
                 composerPortraitUrl = server + composer.getString("thumbnail_path");
+                Log.i("URL", composerPortraitUrl);
                 composerName = composer.getString("full_name");
             } catch (JSONException ex) {
                 throw new RuntimeException(ex);
             }
 
-            // ConvertView is the view being recycled as one item in the listView goes out of screen
-            // Only instantiate the convertView when its null
-            if (convertView == null) {
-                convertView = ((LayoutInflater) context
-                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-                        .inflate(layout, parent, false);
-            }
-
             // Set the portrait image
-            final ImageView portraitImage = (ImageView) convertView.findViewById(R.id.portrait);
+            final ImageView portraitImage = (ImageView) parentView.findViewById(R.id.portrait);
 
             ImageLoader imageLoader = VolleySingleton.getInstance(context).getImageLoader();
+            // TODO This command should utilize caching, currently there is noticeable lag when recycling list items
             imageLoader.get(composerPortraitUrl, new ImageLoader.ImageListener() {
                 @Override
                 public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
@@ -147,33 +117,11 @@ public class ComposerView extends ListActivity {
                 // Set the height and width of the image here for resizing
             }, portraitImage.getHeight(), portraitImage.getHeight());
             // Set the information for the text columns
-            ((TextView) convertView.findViewById(R.id.name)).setText(composerName);
+            ((TextView) parentView.findViewById(R.id.name)).setText(composerName);
             // TODO replace the static string with an actual description once the api is updated
-            ((TextView) convertView.findViewById(R.id.description))
+            ((TextView) parentView.findViewById(R.id.description))
                     .setText(ComposerView.composerDescription);
-            return convertView;
-        }
-
-        @Override
-        public int getCount(){
-            return composers.size();
-        }
-
-        @Override
-        public JSONObject getItem(int position) {
-            return composers.get(position);
-        }
-
-        @Override
-        // There may not be a trivial 1 to 1 mapping between the itemId
-        // in the adapter versus the backend storage, this is the mapping function
-        public long getItemId(int position) {
-            try {
-                JSONObject composer = composers.get(position);
-                return composer.getInt("id");
-            } catch (JSONException ex) {
-                throw new RuntimeException(ex);
-            }
+            return parentView;
         }
     }
 }
