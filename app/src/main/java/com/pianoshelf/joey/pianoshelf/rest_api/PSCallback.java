198652a673f3ea767ff4e693f567f13fe00ad867
Joey Zhao <joey@localhost.localdomain>
2016-08-07 19:27:46 -0400

    comment

--- app/src/main/java/com/pianoshelf/joey/pianoshelf/rest_api/PSCallback.java
+++ app/src/main/java/com/pianoshelf/joey/pianoshelf/rest_api/PSCallback.java
@@ -5,6 +5,7 @@ import org.greenrobot.eventbus.EventBus;
 /**
  * Created by joey on 04/05/16.
  * PianoShelfCallback
+ * Only used when both Data and Meta are both unique Objects
  */
 public abstract class PSCallback<T extends RW> extends DeserializeCB<T> {
     @Override
